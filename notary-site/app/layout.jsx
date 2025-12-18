c2d11f218448a9f785e94cfbc9cb1a1a5dc161de
Jeremy1507 <jeremy@papers.ad>
2025-12-18 13:45:09 +0100

    GTM et Plausible: Chargement immediat

--- notary-site/app/layout.jsx
+++ notary-site/app/layout.jsx
@@ -31,59 +31,18 @@ export default function RootLayout({ children }) {
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
-              var gtmLoaded = false;
-              function loadGTM() {
-                if (gtmLoaded) return;
-                gtmLoaded = true;
-                var gtm = document.createElement('script');
-                gtm.async = true;
-                gtm.src = 'https://www.googletagmanager.com/gtm.js?id=GTM-MR7JDNSD';
-                document.head.appendChild(gtm);
-              }
-              var interactionEvents = ['scroll', 'click', 'keydown', 'touchstart', 'mousedown'];
-              var hasInteracted = false;
-              var loadGTMOnInteraction = function() {
-                if (!hasInteracted) {
-                  hasInteracted = true;
-                  loadGTM();
-                  interactionEvents.forEach(function(event) {
-                    document.removeEventListener(event, loadGTMOnInteraction, { passive: true });
-                  });
-                }
-              };
-              interactionEvents.forEach(function(event) {
-                document.addEventListener(event, loadGTMOnInteraction, { passive: true, once: true });
-              });
-              setTimeout(function() {
-                if (!gtmLoaded) {
-                  loadGTM();
-                }
-              }, 10000);
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MR7JDNSD');
             `,
           }}
         />
         <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              if ('requestIdleCallback' in window) {
-                requestIdleCallback(function() {
-                  var script = document.createElement('script');
-                  script.defer = true;
-                  script.setAttribute('data-domain', 'mynotary.io');
-                  script.src = 'https://plausible.io/js/script.js';
-                  document.head.appendChild(script);
-                }, { timeout: 3000 });
-              } else {
-                setTimeout(function() {
-                  var script = document.createElement('script');
-                  script.defer = true;
-                  script.setAttribute('data-domain', 'mynotary.io');
-                  script.src = 'https://plausible.io/js/script.js';
-                  document.head.appendChild(script);
-                }, 2000);
-              }
-            `,
-          }}
+          defer
+          data-domain="mynotary.io"
+          src="https://plausible.io/js/script.js"
         />
         <script
           dangerouslySetInnerHTML={{
